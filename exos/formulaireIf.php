a94f6d166a71cb9bde1f684fdd6cf717a33c1023
LucasDeluge <deluge999@gmail.com>
2022-06-15 09:49:38 +0200

    maj

--- exos/formulaireIf.php
+++ exos/formulaireIf.php
@@ -1,6 +1,33 @@
 <?php
-if(isset($_POST['nombre']))
- var_dump($_POST);
+// Détermine si une variable est déclarée et est différente de null et de ""
+// if(isset($_POST['nombre']) && $_POST['nombre'] !== ""){
+//     $nombre = intval($_POST['nombre']);
+//     if($nombre>0){
+//         echo "Positif";
+//     } elseif ( $nombre === 0){
+//         echo "vaut 0";
+//     } else {
+//         echo "Négatif";
+//     }
+    
+ // var_dump($_POST);
+// }
+if(isset($_POST['nombre']) && $_POST['nombre'] !== ""){
+    $nombre = intval($_POST['nombre']);
+    if( $nombre >= 6 && $nombre <= 7){
+        echo "Poussin";
+    } elseif ( $nombre >= 8 && $nombre <= 9){
+        echo "Pupille";
+    } elseif ( $nombre >= 10 && $nombre <= 11){
+        echo "Minime";
+    } elseif ($nombre > 12){
+        echo "Cadet";
+    } elseif($nombre < 6) {
+        echo "Enfant";
+    }
+    
+ // var_dump($_POST);
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +40,7 @@ if(isset($_POST['nombre']))
 </head>
 <body>
 <form action="" method="post">
- <label for="nombre">Nombre
+ <label for="nombre">Nombre : 
     <input type="number" name="nombre" placeholder="Donnez un nombre" required>
  </label>
 <button type="submit">Vérifier</button>
